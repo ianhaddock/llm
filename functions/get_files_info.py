6378140a7ac79abc076a0e686dcf8d029b4ba68b
ian <ianh@mac.com>
2025-06-17 18:51:06 +0000

    ch2L2 after checks

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -5,18 +5,23 @@ import os
 def get_files_info(working_directory, directory=None) -> str:
     
     working_path = os.path.abspath(working_directory) 
-    target = os.path.join(working_path, directory)
+    target = working_path
+    if directory:
+        target = os.path.abspath(os.path.join(working_path, directory))
     #print(working_path)
     #print(target)
 
-    if working_path not in target or "../" in directory:
+    if not target.startswith(working_path):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
     if not os.path.isdir(target):
         return f'Error: "{directory}" is not a directory.'
 
     return_string = ''
-    with os.scandir(target) as target:
-        for item in target:
-            return_string += f'{item.name}: file_size: {item.stat().st_size} bytes, is_dir={item.is_dir()}\n'
-    return return_string
+    try:
+        with os.scandir(target) as target:
+            for item in target:
+                return_string += f'{item.name}: file_size: {item.stat().st_size} bytes, is_dir={item.is_dir()}\n'
+        return return_string
+    except Exception as e:
+        return f"Error listing files: {e}"
